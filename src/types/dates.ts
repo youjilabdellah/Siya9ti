cec7873e0dbfc1e5d338aede8f2e413b76b00ab5
youjilabdellah <youjilabdellah@gmail.com>
2026-05-18 10:30:23 +0100

    remove unused files

--- src/types/dates.ts
+++
@@ -1,5 +0,0 @@
-import { MarkingProps } from 'react-native-calendars/src/calendar/day/marking';
-
-export type MarkedDatesType = {
-    [key: string]: MarkingProps;
-};
--- src/types/member.ts
+++
@@ -1,6 +0,0 @@
-export type Member = {
-    id: number;
-    firstName: string;
-    lastName: string;
-    image?: string;
-};
